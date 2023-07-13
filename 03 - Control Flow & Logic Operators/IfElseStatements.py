330aad479cddfdd708492bb80fcc4715c2727247
Yeasir H <yeasir01@gmail.com>
2023-07-13 02:32:26 -0700

    Day 2 Getting Late Commit

---
+++ 03 - Control Flow & Logic Operators/IfElseStatements.py
@@ -0,0 +1,18 @@
+
+#Simple if Else Statement
+boolean = True
+
+if boolean == True:
+    print("The boolean variable is set to True")
+else:
+    print("The boolean variable is set to False")
+
+#Tub Example
+print("\nTub overfill example")
+
+water_level = 81
+
+if water_level >= 80:
+    print("The water in the tub is currently draining!")
+else:
+    print("Water ok.")
---
+++ 03 - Control Flow & Logic Operators/RollerCoaster.py
@@ -0,0 +1,28 @@
+#Nested if else statements
+
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+    
+    wants_photo = input("Would you like to add a photo for $3? Y or N. ")
+
+    if wants_photo == "Y":
+        bill += 3
+        print(f"Your total is ${bill}")
+    else:
+        print(f"Your total is ${bill}")
+else:
+    print("Sorry, you have to be a bit taller to ride.")
