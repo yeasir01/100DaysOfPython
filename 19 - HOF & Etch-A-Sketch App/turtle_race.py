47c64f43c490e3ea81de60f511113a67c7c78f02
Yeasir H <yeasir01@gmail.com>
2023-08-29 00:20:23 -0700

    Day 19 Complete

--- 19 - HOF & Etch-A-Sketch App/turtle_race.py
+++ 19 - HOF & Etch-A-Sketch App/turtle_race.py
@@ -10,34 +10,16 @@ screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen.title("Py-Turtle Racer")
 
 config = [
-    {
-        "color": "red",
-        "y_coordinate": -167
-    },
-    {
-        "color": "blue",
-        "y_coordinate": -100
-    },
-    {
-        "color": "green",
-        "y_coordinate": -33
-    },
-    {
-        "color": "purple",
-        "y_coordinate": 33
-    },
-    {
-        "color": "gray",
-        "y_coordinate": 100
-    },
-    {
-        "color": "pink",
-        "y_coordinate": 167
-    },
+    {"color": "red", "y_coordinate": -167},
+    {"color": "blue", "y_coordinate": -100},
+    {"color": "green", "y_coordinate": -33},
+    {"color": "purple", "y_coordinate": 33},
+    {"color": "gray", "y_coordinate": 100},
+    {"color": "pink", "y_coordinate": 167},
 ]
 
 turtles = []
-#list Comprehension W/Join method to convert to comma separated string.
+# list Comprehension W/Join method to convert to comma separated string.
 colors = ", ".join([item["color"] for item in config])
 
 for item in config:
@@ -47,7 +29,9 @@ for item in config:
     t.goto(-250 + TURTLE_WIDTH, item["y_coordinate"])
     turtles.append(t)
 
-choice = screen.textinput("Question", f"Which color will win?\n{colors}\nEnter a color:")
+choice = screen.textinput(
+    "Question", f"Which color will win?\n{colors}\nEnter a color:"
+)
 
 is_playing = True
 wining_color = None
@@ -56,12 +40,12 @@ while is_playing:
     random_turtle = random.choice(turtles)
     position = random_turtle.xcor()
 
-    if position < (SCREEN_WIDTH/2) - TURTLE_WIDTH:
+    if position < (SCREEN_WIDTH / 2) - TURTLE_WIDTH:
         rand_distance = random.randint(0, 10)
         random_turtle.forward(rand_distance)
     else:
-       is_playing = False
-       wining_color = random_turtle.color()[1]
+        is_playing = False
+        wining_color = random_turtle.color()[1]
 
 if wining_color == choice:
     print(f"You won! The {wining_color} turtle is the winner!")
